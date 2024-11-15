672e66f927e41ba8cf3d7937ee9787705c450ede
DanyelleGiraldo <danyellesgiraldoj@gmail.com>
2024-11-15 10:12:32 -0500

    new file:   src/components/landingpage/conexion.js

---
+++ src/components/landingpage/conexion.js
@@ -0,0 +1,24 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'danilo',
+  password: '9617',
+  database: 'tienda'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Error conectando a la base de datos:', err.stack);
+    return;
+  }
+  console.log('Conectado a la base de datos con el id ' + connection.threadId);
+});
+
+// Hacer una consulta
+connection.query('SELECT * FROM usuarios', (error, results, fields) => {
+  if (error) throw error;
+  console.log('Los resultados son: ', results);
+});
+
+connection.end();
